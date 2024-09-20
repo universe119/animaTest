1e79918788b7d4392dcfe45aa6ce4e1e0265be0c
universe119 <ruddh119@gmail.com>
2024-09-20 10:48:07 +0900

    margin, padding 속성 추가시 주의사항

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ btn.addEventListener("click", () => {
 	// 모션시간인 duration은 숫자만입력(ms단위) 디폴트값 : 500
 	// opacity 제어 : 0 ~ 1 사이의 실수값
 	// 제어 가능 속성 : width, height, opacity, top,left,bottom,left,color,backgroundColor,scroll
+	// margin, padding도 모션 처리 가능 (단 여백관련 속성은 %값 적용 불가, px단위로만 변경 가능)
+	// 속성명은 camel case 규칙 적용하거나 아니면 ""로 감싸줌 (예: margin-left는 marginLeft 혹은 "margint-left"식으로 사용)
 	new Anime(
 		box,
 		{
